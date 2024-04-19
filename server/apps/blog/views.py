0cbfe72b59c58f4b8ecb4308c7d7427d16d2a204
wastered <nonamelite@mail.ru>
2024-04-19 21:14:17 +0300

    Добавлена постраничная разбивка в blog

--- server/apps/blog/views.py
+++ server/apps/blog/views.py
@@ -1,10 +1,24 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    posts_list = Post.published.all()
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если page_number не целое число, то выдать первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если page_number находится вне диапазона,
+        # то выдать последнюю страницу.
+        # Получаем общее число страниц посредством paginator.num_pages
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   "blog/post/list.html",
                   {'posts': posts})
