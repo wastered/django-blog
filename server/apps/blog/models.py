6122f027fc899b4afc9a20f6c5b5180cd8b2a666
wastered <nonamelite@mail.ru>
2024-04-19 20:50:17 +0300

    Усовершенствование блога за счет продвинутых функциональностей

--- server/apps/blog/models.py
+++ server/apps/blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
@@ -40,5 +40,8 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
-                       args=[self.id]
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug]
                        )
