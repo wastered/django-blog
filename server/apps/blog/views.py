e7a26f6193781cb93d9b000c4402a9b699a34c7f
wastered <nonamelite@mail.ru>
2024-07-12 17:04:31 +0300

    Использование представления на основе класса

--- server/apps/blog/views.py
+++ server/apps/blog/views.py
@@ -1,27 +1,17 @@
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from .models import Post
 
 
-def post_list(request):
-    posts_list = Post.published.all()
-    # Постраничная разбивка с 3 постами на страницу
-    paginator = Paginator(posts_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # Если page_number не целое число, то выдать первую страницу
-        posts = paginator.page(1)
-    except EmptyPage:
-        # Если page_number находится вне диапазона,
-        # то выдать последнюю страницу.
-        # Получаем общее число страниц посредством paginator.num_pages
-        posts = paginator.page(paginator.num_pages)
-    return render(request,
-                  "blog/post/list.html",
-                  {'posts': posts})
+class PostListView(ListView):
+    """
+    Представление списка постов
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
 
 def post_detail(request, year, month, day, post):
