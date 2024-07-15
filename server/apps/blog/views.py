905b3aad8e48e4b3de8cfffbd34aa9fee7694d83
wastered <nonamelite@mail.ru>
2024-07-15 19:45:01 +0300

    Добавлено извление постов по сходству

--- server/apps/blog/views.py
+++ server/apps/blog/views.py
@@ -1,5 +1,6 @@
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
@@ -62,9 +63,17 @@ def post_detail(request, year, month, day, post):
     # Форма для комментирования пользователями
     form = CommentForm()
 
+    # Список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post, 'comments': comments, 'form': form})
+                  context={'post': post,
+                           'comments': comments,
+                           'form': form,
+                           'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
