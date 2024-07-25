a43224ea79c7ce6b082731f6c625b6382f1f2ff0
wastered <nonamelite@mail.ru>
2024-07-25 17:40:52 +0300

    Создана новостная лента для постов блога

--- server/apps/blog/urls.py
+++ server/apps/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 urlpatterns = [
@@ -13,4 +14,5 @@ urlpatterns = [
          name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
